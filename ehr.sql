64f9b1d91b9a3ae324c7ec3ad44426e95395b503
Johnson Clever <94965887+cleverjohnson@users.noreply.github.com>
2023-01-26 10:16:04 +0100

    Add files via upload

--- ehr.sql
+++ ehr.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 18, 2023 at 05:33 PM
+-- Generation Time: Jan 25, 2023 at 08:55 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.5
 
@@ -37,7 +37,8 @@ CREATE TABLE `doctors` (
 
 INSERT INTO `doctors` (`FirstName`, `LastName`, `UserName`, `Email`, `Password`, `CPassword`, `Phone_nr`) VALUES
 ('d', 'd', 'dd', 'a@gmail.com', '123', '123', 12),
-('d', 'c', 'dc', 'a@gmail.com', '147', '147', 12);
+('d', 'c', 'dc', 'a@gmail.com', '147', '147', 12),
+('f', 'f', 'ff', 'f@gmail.com', '456', '456', 45);
 
 -- --------------------------------------------------------
 
@@ -47,10 +48,22 @@ INSERT INTO `doctors` (`FirstName`, `LastName`, `UserName`, `Email`, `Password`,
 
 CREATE TABLE `patients` (
   `Name` varchar(50) NOT NULL,
-  `Email-p` varchar(50) NOT NULL,
+  `Emailp` varchar(50) NOT NULL,
   `Address` varchar(50) NOT NULL,
   `Insurance` varchar(50) NOT NULL,
-  `Phone-p` int(50) NOT NULL,
+  `Phonep` int(50) NOT NULL,
   `Gender` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patients`
+--
+
+INSERT INTO `patients` (`Name`, `Emailp`, `Address`, `Insurance`, `Phonep`, `Gender`) VALUES
+('Madalina', 'mada@gmail.com', 'Balti', '1', 1, '1'),
+('Madali', 'mada@gmail.com', 'Balti', '2', 1, '1'),
+('Madali', 'mada@gmail.com', 'Balti', '2', 1, '1'),
+('cd', 'cd@gmail.com', 'ds', '', 45, 'on'),
+('cd', 'cd@gmail.com', 'dad', '', 50, 'on'),
+('cd', 'cd@gmail.com', 'dad', '', 50, 'on');
 COMMIT;
